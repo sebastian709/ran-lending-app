7525fdb8335d5ea86dfc3c85ad85e2a12b798a79
ASL <sebastianjabson07@gmail.com>
2025-07-08 21:49:13 +0800

    test

--- public/js/blogpost.js
+++ public/js/blogpost.js
@@ -556,7 +556,8 @@ $(document).on('click', '.btn-bp-status', function (e) {
   const postId = $btn.data('bp_id');
   const newStatus = $btn.data('status');
   const capitalized = newStatus.charAt(0).toUpperCase() + newStatus.slice(1);
-  console.log('test authasdadsadsadsadsads asasddasdsadasadsadsadsadsadsadsadsads');
+  console.log('test authasdadsadsadsadsads');
+  console.log('test test');
   Swal.fire({ 
     title: `Move to ${capitalized}?`,
     text: `Do you want to move this post to ${newStatus}?`,
