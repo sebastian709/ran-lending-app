3b887aa9cdec51c1cc511f369478220a00b04357
Alejandro Bermudo <drobermudo@gmail.com>
2025-07-11 19:05:15 +0800

    Active loan page

--- public/js/borrower.js
+++ public/js/borrower.js
@@ -149,3 +149,24 @@ $(function () {
     updateLoanSummary();
     updateAdminLoanSummary();
 });
+
+//Set current time
+ $(document).ready(function () {
+    function updateDateTime() {
+        const now = new Date();
+        const options = { 
+            year: 'numeric', 
+            month: 'long', 
+            day: 'numeric', 
+            hour: '2-digit', 
+            minute: '2-digit', 
+            second: '2-digit',
+            hour12: true
+        };
+        const formattedDateTime = now.toLocaleString('en-US', options);
+        $('#current-date').text(formattedDateTime);
+    }
+
+    updateDateTime();
+    setInterval(updateDateTime, 1000);
+});
