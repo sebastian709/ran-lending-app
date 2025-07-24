428b3d083172e8de4a58fa730808867ea5ebda64
ASL <sebastianjabson07@gmail.com>
2025-07-24 23:35:19 +0800

    fetch government type

---
+++ app/Models/GovernmentType.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GovernmentType extends Model
+{
+    protected $fillable = [
+        'id',
+        'government_id_type'
+    ];
+}
