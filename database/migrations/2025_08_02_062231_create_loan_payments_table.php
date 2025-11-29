460136edfc1482572c0a732d1a03824cfd724764
Lordanniel23 <75739948+Lordanniel23@users.noreply.github.com>
2025-11-29 19:55:41 +0800

    Update 2025_08_02_062231_create_loan_payments_table.php

--- database/migrations/2025_08_02_062231_create_loan_payments_table.php
+++ database/migrations/2025_08_02_062231_create_loan_payments_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->text('remarks')->nullable();
             $table->text('attachment');
             $table->integer('added_by')->index(); 
-            $table->dateTime('cancelled_approved_date'); 
+            $table->dateTime('cancelled_approved_date')->nullable(); 
             $table->timestamps();
         });
     }
