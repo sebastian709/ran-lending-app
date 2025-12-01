5b00ff3d4fed3d4b58194ac55e06bfe3a5247fb5
Alejandro Bermudo <drobermudo@gmail.com>
2025-12-01 14:14:23 +0800

    dropdown issue fix

--- resources/views/borrower/app.blade.php
+++ resources/views/borrower/app.blade.php
@@ -162,6 +162,8 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.4/js/lightbox.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
     @yield('scripts')
     @stack('sb-scripts')
 
